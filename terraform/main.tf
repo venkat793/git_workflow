f22ee0baf0f80c4b354b06a059842a86c7f0c280
Venkata Vegi <venkata.vegi@bizacuity.local>
2024-10-16 15:12:01 +0530

    initital commit

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.71.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "statebucket777"
+    key            = "terraform/state.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "table7889"
+    encrypt        = true
+  }
+}
