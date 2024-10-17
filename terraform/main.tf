df9305adb53aef2579061de3ec0adeaeeb10762e
Venkata Vegi <venkata.vegi@bizacuity.local>
2024-10-17 15:51:33 +0530

    0000000000

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ data "aws_iam_policy_document" "allow_public_access" {
     ]
 
     resources = [
-      "${aws_s3_bucket.example.arn}/*"
+      "${data.aws_s3_bucket.example.arn}/*"
     ]
   }
 }
